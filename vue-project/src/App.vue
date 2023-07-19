a66a692162cb2b1a899245a45ae7825fb76053c8
Daniel Reynaud <reynauddaniel26@gmail.com>
2023-07-19 09:40:43 +0000

    Add Styling for component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,18 +5,10 @@ import PageHome from '@/components/PageHome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+    <div class="container">
       <PageHome />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
--- vue-project/src/components/PageHome.vue
+++ vue-project/src/components/PageHome.vue
@@ -1,15 +1,46 @@
 <template>
-    <div>Hello from page home</div>
-    <div v-for="thread in threads" :key="thread.id">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{userById(postById(postId).userId).name}}</p>
-        <p>{{postById(postId).text}}</p>
+<div v-for="thread in threads"
+       :key="thread.id"
+       class="col-large push-top"
+  >
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div class="post"
+           v-for="postId in thread.posts"
+           :key="postId"
+      >
+
+        <div class="user-info">
+          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{postById(postId).text}}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{postById(postId).publishedAt}}
+        </div>
+
       </div>
+
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
+<script setup>
     import sourceData from '@/data.json'
     import { ref } from 'vue'
 
@@ -24,8 +55,8 @@
     function userById (userId) {
         return users.value.find(p => p.id === userId)
     }
-  </script>
+</script>
   
-  <style scoped>
+<style scoped>
   
-  </style>
+</style>
