8f4df3fffe7d3437bc0c094379d7bad496b471cd
Daniel Reynaud <reynauddaniel26@gmail.com>
2023-07-19 12:32:49 +0000

    Refactor ThreadList Component with the Composition API

--- vue-project/src/components/PageHome.vue
+++ vue-project/src/components/PageHome.vue
@@ -1,49 +1,14 @@
 <template>
-    <div class="container">
-<div v-for="thread in threads"
-       :key="thread.id"
-       class="col-large push-top" >
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div class="post"
-           v-for="postId in thread.posts"
-           :key="postId"
-      >
-
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
-
-          <a href="#">
-            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{postById(postId).text}}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{postById(postId).publishedAt}}
-        </div>
-
-      </div>
-
-    </div>
+  <div class="container" style="margin:auto">
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads"/>
   </div>
-</div>
 </template>
   
 <script setup>
     import sourceData from '@/data.json'
     import { ref } from 'vue'
+    import ThreadList from '@/components/ThreadList.vue'
 
     const threads = ref(sourceData.threads)
     const posts = ref(sourceData.posts)
